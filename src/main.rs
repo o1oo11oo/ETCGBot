0fe7ee4c6660a41bd82e1df9883781c5cd37e136
Lukas Fischer <git@o1oo11oo.de>
2022-11-09 14:56:59 +0100

    Update bot for CM Istanbul
    Change texts to include the link to the form and modify the goodbye
    command so it can only be used by o1oo11ooo1ooo11o.

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,10 @@ async fn answer(
     command: Command,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     // try to find out who sent this message, feels like there's a better way to do this
+    let mut sender_id = None;
     let sender = if let MessageKind::Common(common) = &message.kind {
         if let Some(user) = &common.from {
+            sender_id = Some(user.id);
             if let Some(last_name) = &user.last_name {
                 format!("{} {last_name}", user.first_name)
             } else {
@@ -51,12 +53,12 @@ async fn answer(
             log::info!("ETCG message received: {message:?}");
 
             let text = format!(
-                "*__Congratulations\\!__*\n\
+                "Do you want to order your very own cards for the *ESTIEM Trading Card Game*? \
+                Then sign up by using [this form]\
+                (https://docs.google.com/forms/d/e/1FAIpQLSdZFf_fOO7uaA8JKeDUwPCRo4e-S-RnPklU_syOGXCMPll9xA/viewform)\
+                \\!\n\
                 \n\
-                You have successfully used the ETCGBot to apply for the *ESTIEM Trading Card Game*\\. \
-                Feel free to bother @erikviktor at CM if he has no cards for you, \
-                but make sure to pay a lot of money to his PayPal account first \
-                \\(unless you're turkish, swedish, finnish or serbian\\)\\.\n\
+                If you have any questions, feel free to bother @erikviktor here or personally at CM\\.\n\
                 \n\
                 _This message was sent after actions made by {}\\. If you receive it by a mistake, \
                 please submit a pull request [on GitHub](https://github.com/o1oo11oo/ETCGBot)\\._",
@@ -65,6 +67,7 @@ async fn answer(
 
             bot.parse_mode(ParseMode::MarkdownV2)
                 .send_message(message.chat.id, text)
+                .disable_web_page_preview(true)
                 .await?;
         }
 
@@ -72,27 +75,44 @@ async fn answer(
         Command::Goodbye => {
             log::info!("GoodBye message received: {message:?}");
 
-            // craft goodbye message
-            let text = format!(
-                "This joke has probably run its course\\.\n\
-                \n\
-                Thank you everyone for using the ETCGBot, \
-                have fun with the *ESTIEM Trading Card Game*, \
-                have a safe trip to Belgrade and see you there\\!\n\
-                \n\
-                _This message was sent after actions made by {}\\. If you receive it by a mistake, \
-                please submit a pull request [on GitHub](https://github.com/o1oo11oo/ETCGBot)\\._",
-                escape(&sender)
-            );
+            // only leave the chat if sent by @o1oo11ooo1ooo11o
+            if sender_id == Some(709158714) {
+                // craft goodbye message
+                let text = format!(
+                    "Time to leave again\\.\n\
+                    \n\
+                    Thank you everyone for using the ETCGBot, \
+                    and have fun with the *ESTIEM Trading Card Game*\\!\n\
+                    \n\
+                    _This message was sent after actions made by {}\\. If you receive it by a mistake, \
+                    please submit a pull request [on GitHub](https://github.com/o1oo11oo/ETCGBot)\\._",
+                    escape(&sender)
+                );
 
-            // send the message
-            bot.clone()
-                .parse_mode(ParseMode::MarkdownV2)
-                .send_message(message.chat.id, text)
-                .await?;
+                // send the message
+                bot.clone()
+                    .parse_mode(ParseMode::MarkdownV2)
+                    .send_message(message.chat.id, text)
+                    .await?;
 
-            // leave the group
-            bot.leave_chat(message.chat.id).await?;
+                // leave the group
+                bot.leave_chat(message.chat.id).await?;
+            } else {
+                // craft warning message
+                let text = format!(
+                    "Sorry, you are not allowed to use this command\\!\n\
+                    \n\
+                    _This message was sent after actions made by {}\\. If you receive it by a mistake, \
+                    please submit a pull request [on GitHub](https://github.com/o1oo11oo/ETCGBot)\\._",
+                    escape(&sender)
+                );
+
+                // send the message
+                bot.parse_mode(ParseMode::MarkdownV2)
+                    .send_message(message.chat.id, text)
+                    .disable_web_page_preview(true)
+                    .await?;
+            }
         }
     };
 
@@ -107,6 +127,16 @@ async fn main() {
 
     pretty_env_logger::init();
     log::info!("Starting ETCGBot...");
+
+    if std::env::var("TELOXIDE_TOKEN").is_err() {
+        log::warn!("No API token found, set it as TELOXIDE_TOKEN or enter it here: ");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("error: unable to read user input");
+        std::env::set_var("TELOXIDE_TOKEN", input);
+        log::info!("API token received, resuming startup...");
+    }
 
     let bot = Bot::from_env().auto_send();
 
